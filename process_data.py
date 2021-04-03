d8e8026afef74cd2c6f641ec3e60791d6bfb5083
MiranAlMehrab <bsse0931@iit.du.ac.bd>
2021-04-03 23:20:34 +0600

    added few examples

---
+++ process_data.py
@@ -0,0 +1,29 @@
+import eval
+import pickle
+import hashlib
+
+def insecure_code_executaion():
+    user_exp = input("enter your code to execute: ")
+    eval(user_exp)
+
+
+def insecure_data_deserialization():
+    uploaded_file = get_file_from_user()
+    user_data = pickle.load(uploaded_file)
+
+
+def insecure_yaml_operation():
+    uploaded_config_file = get_file_from_user()
+    config_data = yaml.load(uploaded_config_file)
+
+
+def use_of_insecure_cipher():
+    hashed_password = hashlib.md5(password.encode())
+
+def user_sql_statement():
+    product_id = input()
+    query = f"delete from products where id = {product_id}"
+
+
+def send_data_as_safe_marked():
+    mark_safe(unicode(internet_downloaded_content))
